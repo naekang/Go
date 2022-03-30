64b02f3ef811864884b957031e8cfabf1521ef7d
naekang <rlawlsgh6306@gmail.com>
2022-03-30 11:38:48 +0900

    BOJ 10039 Go

---
+++ boj/boj10039/boj10039.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var score, sum int
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for i := 0; i < 5; i++ {
+		fmt.Fscanln(reader, &score)
+		if score < 40 {
+			score = 40
+		}
+		sum += score
+	}
+
+	fmt.Println(sum / 5)
+}
