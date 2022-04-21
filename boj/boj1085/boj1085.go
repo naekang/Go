a4ecb19831c73a0a6f9924e43144ae10fadaecec
naekang <rlawlsgh6306@gmail.com>
2022-04-21 10:51:24 +0900

    BOJ 1085 Go

---
+++ boj/boj1085/boj1085.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+func main() {
+	var x, y, w, h float64
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Fscanln(reader, &x, &y, &w, &h)
+
+	fmt.Println(math.Min(math.Min(x, w-x), math.Min(y, h-y)))
+}
